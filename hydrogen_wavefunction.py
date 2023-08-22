809b74ac2cb28aedb4380bc932728ce4720cdf58
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2023-08-21 23:00:48 -0500

    Added angular conttribution function (1stIteration)

--- hydrogen_wavefunction.py
+++ hydrogen_wavefunction.py
@@ -2,34 +2,32 @@ import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import seaborn as sb
-import math
 
-""" Computes the Radial Contribution of the Wavefunction Using Exponential Decay, Laguerre Polynomials, and Power Term
+""" Computes the Angular Contribution of the Wavefunction Using Exponential Decay, Legendre Polynomials, and Power Term
 
-Arguments: (int) n - Principal Quantum Number, the energy level or shell. (r in spherical coordinates)
-           (int) l - Angular Momentum Quantum Number, the subshell or angular dependence of the orbital. (Θ in spherical coordinates)
-           (numpy array) r - Radial Coordinate, from a given polar/spherical coordinate. 
+Arguments: (int) l - Angular Momentum Quantum Number, the subshell or angular dependence of the orbital. (Θ in spherical coordinates)
+           (int) ml - Magnetic Quantum Number, the orientation in space of an orbital (ϕ in spherical coordinates)
+           (numpy array) theta - Polar Angle Coordinate, from a given polar/spherical coordinate. 
+           (int) phi - Azimuth Angle Coordinate, from a given polar/spherical coordinate. 
            
-Output: A numpy array of the radial contribution to the wavefunction
+           *Note, that we only analyize the REAL portion of the complex spherical harmonic equation
+
+Output: A numpy array of the angular contribution (spherical harmonics) of the wavefunction
 """
-def radial_Contribution(n, l, r):
+def angular_Contribution(l, ml, theta, phi):
+
+    e = np.e
+    pi = np.pi
+
+    negPosTerm = (-1)**ml
+
+    sqaureRootTerm = np.sqrt(
+                            (((2 * l) + 1)/(4 * pi)) *
+                            ((np.math.factorial(l - ml))/(np.math.factorial(l + ml)))
+                            )
     
-    e = math.e
-    a0 = sp.constants.physical_constants["Bohr radius"][0] * 1e+12
-    z = 2 / (n * a0)
+    legendreTerm = sp.special.lpmv(ml, l, np.cos(theta))
 
-    squareRootTerm = (-1) * np.sqrt(
-                                   (z ** 3) *
-                                   ((np.math.factorial(n - l - 1)) / (2 * n * (np.math.factorial(n + l))))
-                                   )
-    
-    powerOfLTerm = (z * r) ** l
+    decayTerm = np.real(np.exp(1j * ml * phi))
 
-    laguerreTerm = sp.special.genlaguerre((n - l - 1), (2 * l + 1))
-
-    decayTerm = e**((-1)*((z * r) / 2))
-
-    return squareRootTerm * powerOfLTerm * laguerreTerm(z * r) * decayTerm
-
-
-
+    return negPosTerm * sqaureRootTerm * legendreTerm * decayTerm
