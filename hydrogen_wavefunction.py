8f71f0074c34843b42da98245a7aff30d4687249
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2023-08-21 19:16:40 -0500

    Radial contribution function created

--- hydrogen_wavefunction.py
+++ hydrogen_wavefunction.py
@@ -12,12 +12,10 @@ Arguments: (int) n - Principal Quantum Number, the energy level or shell. (r in 
            
 Output: A numpy array of the radial contribution to the wavefunction
 """
-
 def radial_Contribution(n, l, r):
     
     e = math.e
-    a0 = 1
-    a1 = sp.constants.physical_constants["Bohr radius"][0] * 1e+12
+    a0 = sp.constants.physical_constants["Bohr radius"][0] * 1e+12
     z = 2 / (n * a0)
 
     squareRootTerm = (-1) * np.sqrt(
@@ -34,5 +32,4 @@ def radial_Contribution(n, l, r):
     return squareRootTerm * powerOfLTerm * laguerreTerm(z * r) * decayTerm
 
 
-print(radial_Contribution(3,2,100))
 
