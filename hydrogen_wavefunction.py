dead72b5806f5fb0f90046cb7b2a6866b7294df0
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2023-08-22 20:42:41 -0500

    Added wavefunction, wavefunctionprob functions and semi-finished plot function

--- hydrogen_wavefunction.py
+++ hydrogen_wavefunction.py
@@ -1,13 +1,13 @@
 import numpy as np
 import scipy as sp
-import matplotlib as mpl
-import seaborn as sb
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 """ Computes the Radial Contribution of the Wavefunction Using Exponential Decay, Laguerre Polynomials, and Power Term
 
 Arguments: (int) n - Principal Quantum Number, the energy level or shell. (r in spherical coordinates)
            (int) l - Angular Momentum Quantum Number, the subshell or angular dependence of the orbital. (Θ in spherical coordinates)
-           (numpy array) r - Variable Radius Coordinate for seaborn plot 
+           (numpy array) r - Variable Radius Coordinate for plot
            
 Output: A numpy array of the radial contribution to the wavefunction
 """
@@ -34,11 +34,8 @@ def radial_Contribution(n, l, r):
 
 Arguments: (int) l - Angular Momentum Quantum Number, the subshell or angular dependence of the orbital. (Θ in spherical coordinates)
            (int) ml - Magnetic Quantum Number, the orientation in space of an orbital (ϕ in spherical coordinates)
-           (numpy array) theta - Polar Angle Coordinate, from a given polar/spherical coordinate. 
-           (int) phi - Azimuth Angle Coordinate, from a given polar/spherical coordinate. 
-           
-           *Note, that we only analyize the REAL portion (np.real) of the complex spherical harmonic equation as it ascertains the physical probabilities.
-           *Note, if we are looking for the probability densities, will be considered 0 later on since this will envision a top down view of the orbitals in the main plot.
+           (numpy array) theta - Variable Polar Angle Coordinate for plot
+           (numpy array) phi - Variable Azimuth Angle Coordinate for plot
            
 Output: A numpy array of the angular contribution (spherical harmonics) of the wavefunction
 """
@@ -49,14 +46,55 @@ def angular_Contribution(l, ml, theta, phi):
 
     negPosTerm = (-1)**ml
 
+    #Needed to ensure calculation for -l
+    absML = np.abs(ml)
     sqaureRootTerm = np.sqrt(
                             (((2 * l) + 1)/(4 * pi)) *
-                            ((np.math.factorial(l - ml))/(np.math.factorial(l + ml)))
+                            ((np.math.factorial(l - absML))/(np.math.factorial(l + absML)))
                             )
     
     legendreTerm = sp.special.lpmv(ml, l, np.cos(theta))
 
-    decayTerm = np.real(np.exp(1j * ml * phi))
+    decayTerm = np.exp(1.j * ml * phi)
 
     return negPosTerm * sqaureRootTerm * legendreTerm * decayTerm
+    
+""" Computes the total complex wavefunction from the calculated angular_Contribution and radial_Contribution
+
+Arguments: (int) n - Principal Quantum Number, the energy level or shell. (r in spherical coordinates)
+           (int) l - Angular Momentum Quantum Number, the subshell or angular dependence of the orbital. (Θ in spherical coordinates) 
+           (int) ml - Magnetic Quantum Number, the orientation in space of an orbital (ϕ in spherical coordinates)
+
+Output: A COMPLEX numpy array of the total wavefunction
+"""
+
+def complex_Wave_Function(n, l, ml):     
+
+    pi = np.pi
+
+    num_points = 680
+    r = np.linspace(0, 2, num_points)
+    theta = np.linspace(0, 2 * pi, num_points)
+    phi = np.linspace(0, pi, num_points)
+    
+    theta, phi = np.meshgrid(theta, phi)
+
+    radial = radial_Contribution(n, l, r)
+    angular = angular_Contribution(l, ml, theta, phi)
+
+    psi = radial * angular
+
+    return psi
+    
+def probability_Density(psi):
+
+    return (np.abs(psi) ** 2)
+
+def main_Plot(P):
+    
+    plt.imshow(P, interpolation='none')
+    plt.show()
+
+main_Plot(probability_Density(complex_Wave_Function(3,1,1)))
+
     
